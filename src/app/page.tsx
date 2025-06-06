54500b371c08f3f924330f43e5b39b8148296156
Utkarsha Patil <utkarsha.ajay.patil@gmail.com>
2025-06-06 02:17:06 +0000

    education should be on left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,15 @@ export default function AboutMePage() {
 
       <div className="mt-16 md:mt-24 grid md:grid-cols-3 gap-8 lg:gap-12">
         <section className="md:col-span-1">
+          <PageTitle title="Education" icon={<GraduationCap size={36} strokeWidth={1.5}/>} className="mb-6 md:mb-8" />
+          <div className="space-y-6">
+            {education.map((edu) => (
+              <EducationItem key={edu.id} education={edu} />
+            ))}
+          </div>
+        </section>
+
+        <section className="md:col-span-2">
           <PageTitle title="My Skills" icon={<Brain size={36} strokeWidth={1.5}/>} className="mb-6 md:mb-8" />
           <Card className="shadow-lg h-full">
             <CardContent className="px-4 pt-4 pb-2">
@@ -59,15 +68,6 @@ export default function AboutMePage() {
               </div>
             </CardContent>
           </Card>
-        </section>
-
-        <section className="md:col-span-2">
-          <PageTitle title="Education" icon={<GraduationCap size={36} strokeWidth={1.5}/>} className="mb-6 md:mb-8" />
-          <div className="space-y-6">
-            {education.map((edu) => (
-              <EducationItem key={edu.id} education={edu} />
-            ))}
-          </div>
         </section>
       </div>
     </Container>
